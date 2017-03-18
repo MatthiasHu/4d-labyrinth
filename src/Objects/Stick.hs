d3df136beffb6f98e10ca9ab1f924ae7a02a3a79
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2017-03-18 07:31:11 +0100

    fixed Stick object (top and bottom faces)

--- src/Objects/Stick.hs
+++ src/Objects/Stick.hs
@@ -12,12 +12,17 @@ import Color
 stick :: (Floating a) => Int -> a -> a -> Color -> Object a
 stick n r l c = Object $
   [ Face c (map P
-              [ withZ 0 (r *^ dir i) , withZ 0 (r *^ dir (i+1))
-              , withZ l (r *^ dir (i+1)) , withZ l (r *^ dir i) ])
+              [ bot i     , bot (i+1)
+              , top (i+1) , top i     ])
       (withZ 0 (dir (i+0.5)))
       faceDist
-  | i <- map fromIntegral [0..(n-1)] ]
+  | i <- is ]
+  ++ [ Face c (map (P . bot) is) (V3 0 0 (-1)) 0
+     , Face c (map (P . top) is) (V3 0 0 1   ) l ]
   where
+    bot i = withZ 0 (r *^ dir i)
+    top i = withZ l (r *^ dir i)
+    is = map fromIntegral [0..(n-1)]
     dir i = let phi = i*tau/fromIntegral n
             in V2 (cos phi) (sin phi)
     withZ z (V2 x y) = V3 x y z
