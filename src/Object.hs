5a3c2055bce69163ab2457aea103f12dd9f7745d
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2017-03-23 01:29:19 +0100

    (WIP) vector type variable in Object

--- src/Object.hs
+++ src/Object.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Object
   ( Object(..)
@@ -18,22 +20,19 @@ import Color
 import Transformation
 
 
--- a graphical object,
--- consisting of some faces
-data Object a = Object
-  { _objectCenter :: Point V3 a
+-- A graphical object.
+data Object v a = Object
+  { _objectCenter :: Point v a
   , _objectRadius :: a
-  , _objectFaces :: [Face a]
+  , _objectFaces :: [Face v a]
   }
   deriving (Functor)
 
--- a face of an object,
--- with a color, a list of vertices
--- and a plane normal
-data Face a = Face
+-- A face of an object.
+data Face v a = Face
   { _faceColor :: Color
-  , _faceVertices :: [Point V3 a]
-  , _faceNormal :: V3 a
+  , _faceVertices :: [Point v a]
+  , _faceNormal :: v a
   , _faceDistance :: a
   }
   deriving (Functor)
@@ -41,22 +40,23 @@ data Face a = Face
 makeLenses ''Object
 makeLenses ''Face
 
-instance Transformable Face where
+instance (Metric v, Foldable v) => Transformable v (Face v) where
   transform t f = f &
       (faceVertices . each %~ transform t)
     . (faceNormal %~ transform t)
     . (faceDistance +~ translationPart t `dot`
                        transform t (f ^. faceNormal))
 
-instance Transformable Object where
+instance (Metric v, Foldable v) => Transformable v (Object v) where
   transform t =
       (objectFaces . each %~ transform t)
     . (objectCenter %~ transform t)
 
-boundingInequalities :: Object a -> [(V3 a, a)]
+boundingInequalities :: Object v a -> [(v a, a)]
 boundingInequalities =
   map (\f -> (f ^. faceNormal, f ^. faceDistance)) . view objectFaces
 
-inReach :: (Floating a, Ord a) => Point V3 a -> a -> Object a -> Bool
+inReach :: (Metric v, Floating a, Ord a) =>
+  Point v a -> a -> Object v a -> Bool
 inReach p r o =
   distance p (o ^. objectCenter) <= r + (o ^. objectRadius)
