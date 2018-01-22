c7fd2b8bca3641df3337c41c05576ae55f78f317
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2018-01-22 23:54:10 +0100

    gem in the space-filling tunnel

--- src/Worlds/SpaceFillingTunnel.hs
+++ src/Worlds/SpaceFillingTunnel.hs
@@ -9,6 +9,7 @@ import Data.Foldable
 import qualified Data.Set as Set
 import Control.Monad.Random
 import Linear hiding (translation)
+import Control.Lens
 
 import Constraints.Scalar
 import Constraints.Vector
@@ -16,12 +17,17 @@ import Worlds.RandomColorBox
 import Transformation
 import SceneTO
 import Worlds.SpaceFillingPath
+import Objects.Cube
+import Color
 
 
 spaceFillingTunnel :: forall m v a.
-  (MonadRandom m, SomeVector v, SomeScalar a) =>
+  (MonadRandom m, SomeVector v, R4 v, SomeScalar a) =>
   Int -> m (SceneTO v a, Transformation v a)
-spaceFillingTunnel iteration = (, translation (pure (-1))) <$> scene
+spaceFillingTunnel iteration =
+    (, translation (pure (-1)))
+  . (\s -> SceneFork [s, gem])
+  <$> scene
   where
     scene = randomColorBoxTunnel (2^(iteration+1))
       ((`Set.notMember` pathSet) . toList)
@@ -29,3 +35,6 @@ spaceFillingTunnel iteration = (, translation (pure (-1))) <$> scene
     pathSet = Set.fromList path
     path = map (map (+1)) $ stretchedSpaceFillingPath dim iteration
     dim = length (pure 0 :: v Int)
+    gem = Transformed
+      (translation $ pure 1 ^+^ (zero & _x .~ 2^iteration))
+      (SceneObject $ cube 0.2 white)
